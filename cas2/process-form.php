edff3196ae58a61682dc18b5d310847309f6d2d9
Alan Wani <alan132301@gmail.com>
2025-11-16 20:44:45 +0300

    Add files via upload

---
+++ cas2/process-form.php
@@ -0,0 +1,8 @@
+<?php
+
+$currentDate = $_POST['current-date'];
+$awareness = $_POST['awareness'];
+$understanding = $_POST['understanding'];
+$comment = $_POST['comment'];
+
+var_dump($currentDate, $awareness, $understanding, $comment);
